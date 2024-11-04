9eb5082a4486758335a55f832dc0bed267eb8655
Bijoy Arikkatt Babu <bijoyarikkatt@gmail.com>
2024-11-04 11:17:48 +0000

    initial commit

---
+++ AsyncImage/AsyncImage/AsyncImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncImageApp.swift
+//  AsyncImage
+//
+//  Created by Bijoy A B on 04/11/24.
+//
+
+import SwiftUI
+
+@main
+struct AsyncImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AsyncImage/AsyncImageTests/AsyncImageTests.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncImageTests.swift
+//  AsyncImageTests
+//
+//  Created by Bijoy A B on 04/11/24.
+//
+
+import Testing
+@testable import AsyncImage
+
+struct AsyncImageTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
